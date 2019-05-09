2dc809847a7464d9a2f2171390229cc63a14851e
Gnu-Bricoleur <sylvain.migaud@gmail.com>
2019-05-09 23:21:21 +0100

    PID en cours

--- Soft/MainNucleo/Src/assert.c
+++ Soft/MainNucleo/Src/assert.c
@@ -22,57 +22,60 @@ void updatePos(int tickRight, int tickGauche, double* positionX, double* positio
 
 void PID(double* positionX, double* positionY, double* angle, int* moteurR, int* dirR, int* moteurL, int* dirR, int targetX, int targetY, int targetAngle)
 {
-    //On calcule la distance séparant le robot de sa cible
-	distanceCible = sqrt((xC-xR)*(xC-xR)+(yC-yR)*(yC-yR));
+    static 
+    //distance between robot and target
+    double distanceTarget = sqrt((targetX-(*positionX))*(targetX-(*positionX))+(targetY-(*positionY))*(targetY-(*positionY)));
+    int signe = 0;
+    //target left or right ?
+    if(positionY > targetY)
+    {
+        signe = 1;
+    }else
+    {
+        signe = -1;
+    }
 
-	//On regarde si la cible est à gauche ou à droite du robot
-	if(yR > yC)
-	{
-		signe = 1;
-	}else
-	{
-		signe = -1;
-	}
+    //On calcule l'angle entre le robot et la cible
+    double consigneOrientation = signe * acos((xC-xR)/((xC-xR)*(xC-xR)*(yC-yR)*(yC-yR));
 
-	//On calcule l'angle entre le robot et la cible
-	consigneOrientation = signe * acos((xC-xR)/((xC-xR)*(xC-xR)*(yC-yR)*(yC-yR));
-						
-	//On détermine les commandes à envoyer aux moteurs
-	cmdD = abs((int)distanceCible);
-	if(cmdD>255)
-	{
-	  	cmdD = 255;
-	}
-	cmdG = cmdD;
-									   
-	//Calcul de l'erreur
-	erreurAngle =  consigneOrientation - orientation;
-									   
-	//Calcul de la différence entre l'erreur au coup précédent et l'erreur actuelle.
-	deltaErreur = erreurAngle - erreurPre;
+    //On détermine les commandes à envoyer aux moteurs
+    int (*moteurR) = abs((int)distanceCible);
+    if((*moteurR)>10000)
+    {
+        (*moteurR) = 10000;
+    }
+    (*moteurL) = (*moteurR);
+   
+    //Calcul de l'erreur
+    double erreurAngle =  consigneOrientation - (*angle);
+   
+    //Calcul de la différence entre l'erreur au coup précédent et l'erreur actuelle.
+    double deltaErreur = erreurAngle - erreurPre;
 
-	//Mise en mémoire de l'erreur actuelle
-	erreurPre  = erreurAngle;
-				
-	//Calcul de la valeur à envoyer aux moteurs pour tourner
-	int detltaCommande = coeffP*erreurAngle + coeffD * deltaErreur;
-									   
-	cmdG += deltaCommande;
-	cmdD -= deltaCommande;
-									   
-	if(cmdD>255)
-	{
-	  	cmdD = 255;
-	}else if(cmdD < 0)
-	{
-	  	cmdD = 0;
-	}
-									   
-	if(cmdG>255)
-	{
-	  	cmdG = 255;
-	}else if(cmdG < 0)
-	{
-	  	cmdG = 0;
-	}
+    //Mise en mémoire de l'erreur actuelle
+    erreurPre  = erreurAngle;
+
+    //Calcul de la valeur à envoyer aux moteurs pour tourner
+    int detltaCommande = coeffP*erreurAngle + coeffD * deltaErreur;
+   
+    (*moteurL) += deltaCommande;
+    (*moteurR) -= deltaCommande;
+   
+    if((*moteurR)>10000)
+    {
+        (*moteurR) = 10000;
+    }else if((*moteurR) < 0)
+    {
+        (*moteurR) = 0;
+    }
+
+    if((*moteurL)>10000)
+    {
+        (*moteurL) = 10000;
+    }else if((*moteurL) < 0)
+    {
+        (*moteurL) = 0;
+    }
+    *dirR = 1;
+    *dirL = 1;
 }
