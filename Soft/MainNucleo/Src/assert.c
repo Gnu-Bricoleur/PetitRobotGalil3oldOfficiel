9e1c45837fa108e785179f4c9cbb6e1b4de7252f
Migaud <Sylvain.Migaud@analog.com>
2019-05-09 16:58:54 +0100

    lci svg

--- Soft/MainNucleo/Src/assert.c
+++ Soft/MainNucleo/Src/assert.c
@@ -18,3 +18,61 @@ void updatePos(int tickRight, int tickGauche, double* positionX, double* positio
   *positionX += dX / TICKSPERMM;
   *positionY += dY / TICKSPERMM;
 }
+
+
+void PID(double* positionX, double* positionY, double* angle, int* moteurR, int* dirR, int* moteurL, int* dirR, int targetX, int targetY, int targetAngle)
+{
+    //On calcule la distance séparant le robot de sa cible
+	distanceCible = sqrt((xC-xR)*(xC-xR)+(yC-yR)*(yC-yR));
+
+	//On regarde si la cible est à gauche ou à droite du robot
+	if(yR > yC)
+	{
+		signe = 1;
+	}else
+	{
+		signe = -1;
+	}
+
+	//On calcule l'angle entre le robot et la cible
+	consigneOrientation = signe * acos((xC-xR)/((xC-xR)*(xC-xR)*(yC-yR)*(yC-yR));
+						
+	//On détermine les commandes à envoyer aux moteurs
+	cmdD = abs((int)distanceCible);
+	if(cmdD>255)
+	{
+	  	cmdD = 255;
+	}
+	cmdG = cmdD;
+									   
+	//Calcul de l'erreur
+	erreurAngle =  consigneOrientation - orientation;
+									   
+	//Calcul de la différence entre l'erreur au coup précédent et l'erreur actuelle.
+	deltaErreur = erreurAngle - erreurPre;
+
+	//Mise en mémoire de l'erreur actuelle
+	erreurPre  = erreurAngle;
+				
+	//Calcul de la valeur à envoyer aux moteurs pour tourner
+	int detltaCommande = coeffP*erreurAngle + coeffD * deltaErreur;
+									   
+	cmdG += deltaCommande;
+	cmdD -= deltaCommande;
+									   
+	if(cmdD>255)
+	{
+	  	cmdD = 255;
+	}else if(cmdD < 0)
+	{
+	  	cmdD = 0;
+	}
+									   
+	if(cmdG>255)
+	{
+	  	cmdG = 255;
+	}else if(cmdG < 0)
+	{
+	  	cmdG = 0;
+	}
+}
